8394f884b327e340ab2cd9950d729d862053b34a
RosGyl <rosita.gyllenhammar@outlook.com>
2025-04-16 10:23:08 +0200

    ändrat bildlänk igen

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className="container text-center py-5">
       <h1 className="mb-4">Välkommen till Tomat Paradiset!</h1>
       <img 
-        src="/webshop6/images/tomatparadis.jpg" // provar igen
+       src={`${process.env.PUBLIC_URL}/images/tomatparadis.jpg`} // provar igen
         alt="Tomatparadiset" 
         className="img-fluid mb-4" 
         style={{ maxHeight: "400px", objectFit: "cover" }}
