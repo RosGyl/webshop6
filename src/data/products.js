47aabd3759f0d91b2596bc37a437d994f0dc13a0
RosGyl <rosita.gyllenhammar@outlook.com>
2025-04-24 10:39:34 +0200

    ny bild till varukorg

--- src/data/products.js
+++ src/data/products.js
@@ -3,98 +3,110 @@ const products = [
     id: 1,
     name: "Bifftomat",
     description: "Bifftomat Coeur De Boeuf",
-    longDescription: "Bifftomater är stora och köttiga, perfekta för att göra såser eller att äta färska i sallader. Denna sort är känd för sin intensiva smak och saftighet.",
+    longDescription:
+      "Bifftomater är stora och köttiga, perfekta för att göra såser eller att äta färska i sallader. Denna sort är känd för sin intensiva smak och saftighet.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/bifftomat.jpg`
+    image: `${process.env.PUBLIC_URL}/images/biff.jpg`,
   },
   {
     id: 2,
     name: "Pärontomat",
     description: "Yellow Pearshaped, Kulturarv",
-    longDescription: "Pärontomater är kända för sin unika form och söta smak. De är perfekta för att ge en extra touch till dina rätter.",
+    longDescription:
+      "Pärontomater är kända för sin unika form och söta smak. De är perfekta för att ge en extra touch till dina rätter.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/paron.jpg`
+    image: `${process.env.PUBLIC_URL}/images/paron.jpg`,
   },
   {
     id: 3,
     name: "Plommontomat",
-    description: "Kulturarv",
-    longDescription: "Plommontomater är kända för sin söta smak och saftiga kött. De är perfekta för att göra såser eller att äta färska.",
+    description: "Herodes, Kulturarv",
+    longDescription:
+      "Plommontomater är kända för sin söta smak och saftiga kött. De är perfekta för att göra såser eller att äta färska.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/plommon.jpg`
+    image: `${process.env.PUBLIC_URL}/images/plommon.jpg`,
   },
   {
     id: 4,
-    name: "Körsbärstomat 'Black Cherry'",
+    name: "Körsbärstomat Black Cherry",
     description: "En ovanlig sort av en vanlig grönsak.",
-    longDescription: "Körsbärstomater är små, söta och perfekta för snacking eller i sallader. Denna sort är känd för sin intensiva smak och saftighet.",
+    longDescription:
+      "Körsbärstomater är små, söta och perfekta för snacking eller i sallader. Denna sort är känd för sin intensiva smak och saftighet.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/97071_1.jpg`
+    image: `${process.env.PUBLIC_URL}/images/97071_1.jpg`,
   },
   {
     id: 5,
     name: "Kösbärstomat",
     description: "F1 Supersweet 100",
-    longDescription: "Körsbärstomater är små, söta och perfekta för snacking eller i sallader. Denna sort är känd för sin intensiva smak och saftighet.",
+    longDescription:
+      "Körsbärstomater är små, söta och perfekta för snacking eller i sallader. Denna sort är känd för sin intensiva smak och saftighet.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/kosbar.jpg`
+    image: `${process.env.PUBLIC_URL}/images/kosbar.jpg`,
   },
   {
     id: 6,
     name: "Lucky Tiger",
     description: "Ekologisk tomat",
-    longDescription: "Lucky Tiger är en sällsynt sort med en unik smak och färg. Den är perfekt för att ge en extra touch till dina rätter.",
+    longDescription:
+      "Lucky Tiger är en sällsynt sort med en unik smak och färg. Den är perfekt för att ge en extra touch till dina rätter.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/tiger.jpg`
+    image: `${process.env.PUBLIC_URL}/images/tiger.jpg`,
   },
   {
     id: 7,
     name: "Ekologisk Busktomat",
     description: "Yellow Donut - Kultursort",
-    longDescription: "Busktomater är perfekta för små utrymmen och ger en riklig skörd av söta tomater. Denna sort är känd för sin unika form och smak.",
+    longDescription:
+      "Busktomater är perfekta för små utrymmen och ger en riklig skörd av söta tomater. Denna sort är känd för sin unika form och smak.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/buskgul.jpg`
+    image: `${process.env.PUBLIC_URL}/images/buskgul.jpg`,
   },
   {
     id: 8,
     name: "Tomat Maja",
     description: "Framtagen i Sverige, utmärkt för det nordiska klimatet",
-    longDescription: "Maja är en sällsynt sort som är perfekt för det nordiska klimatet. Den är känd för sin söta smak och saftiga kött.",
+    longDescription:
+      "Maja är en sällsynt sort som är perfekt för det nordiska klimatet. Den är känd för sin söta smak och saftiga kött.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/litentomat.jpg`
+    image: `${process.env.PUBLIC_URL}/images/litentomat.jpg`,
   },
   {
     id: 9,
     name: "Citrina",
     description: "Citrina, Kultursort",
-    longDescription: "Citrina är en gammal sort som är känd för sin söta smak och saftiga kött. Den är perfekt för att göra såser eller att äta färsk i sallader.",
+    longDescription:
+      "Citrina är en gammal sort som är känd för sin söta smak och saftiga kött. Den är perfekt för att göra såser eller att äta färsk i sallader.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/citrina.jpg`
+    image: `${process.env.PUBLIC_URL}/images/citrina.jpg`,
   },
   {
     id: 10,
     name: "Tomat F1 Harzfeuer",
     description: "Ekologisk tomatFrån 1961.",
-    longDescription: "Harzfeuer är en gammal sort som är känd för sin söta smak och saftiga kött. Den är perfekt för att göra såser eller att äta färsk i sallader.",
+    longDescription:
+      "Harzfeuer är en gammal sort som är känd för sin söta smak och saftiga kött. Den är perfekt för att göra såser eller att äta färsk i sallader.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/tomatparadis.jpg`
+    image: `${process.env.PUBLIC_URL}/images/tomatparadis.jpg`,
   },
   {
     id: 11,
     name: "Bifftomat White Beauty",
     description: "Stora, saftiga, guldgula frukter.",
-    longDescription: "Bifftomat 'White Beauty' är en sällsynt sort med stora, saftiga och guldgula frukter. Den är perfekt för att ge en extra touch till dina rätter.",
+    longDescription:
+      "Bifftomat 'White Beauty' är en sällsynt sort med stora, saftiga och guldgula frukter. Den är perfekt för att ge en extra touch till dina rätter.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/gulbiff.jpg`
+    image: `${process.env.PUBLIC_URL}/images/gulbiff.jpg`,
   },
   {
     id: 12,
-    name: "Plommontomat Taiko'",
+    name: "Plommontomat Taiko",
     description: "Kompakta klasar med långsmala solgula frukter.",
-    longDescription: "Plommontomat 'Taiko' är en sällsynt sort med kompakt växtsätt och långsmala solgula frukter. Den är perfekt för att ge en extra touch till dina rätter.",
+    longDescription:
+      "Plommontomat 'Taiko' är en sällsynt sort med kompakt växtsätt och långsmala solgula frukter. Den är perfekt för att ge en extra touch till dina rätter.",
     price: 50,
-    image: `${process.env.PUBLIC_URL}/images/taiko.jpg`
-  }
+    image: `${process.env.PUBLIC_URL}/images/taiko.jpg`,
+  },
 ];
 
 export default products;
